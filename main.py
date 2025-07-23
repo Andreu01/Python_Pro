65869c7823e1d92bc625153e05ff7b0f42378fbc
Andreu01 <andresrespinozag@hotmail.com>
2025-07-22 18:12:12 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,63 +1,46 @@
-import discord
-from discord.ext import commands
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True  # Activar acceso a la información de los miembros
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-@bot.event
-async def en_linea():
-    print(f'Tu bot {bot.user} esta en linea!')
-    
-@bot.command()
-async def saludar(ctx,*, mensaje:str):
-    
-    mensaje = mensaje.lower().strip()
-    
-    if mensaje == 'hola':
-        
-        await ctx.send('Hola, ¿cómo estás?')
-        
-    elif mensaje == 'que onda':
-        
-        await ctx.send('Todo bien')
-    
-    elif mensaje == 'klk':
-        
-        await ctx.send('Todo bien')
-
-@bot.command()
-async def despedirse(ctx,*, mensaje:str):
-    
-    mensaje = mensaje.lower().strip()
-    
-    if mensaje == 'adios':
-        
-        await ctx.send('Hasta luego')
-        
-    elif mensaje == 'chao me voy':
-        
-        await ctx.send('Que Dios te acompañe')
-    
-    elif mensaje == 'me tengo que ir':
-        
-        await ctx.send('Chao, estoy disponible cuando lo necesites')
-
-@bot.command()
-async def info(ctx, *, member: discord.Member):
-    """Tells you some info about the member."""
-    msg = f'{member} joined on {member.joined_at} and has {len(member.roles)} roles.'
-    await ctx.send(msg)
-
-@info.error
-async def info_error(ctx, error):
-    if isinstance(error, commands.BadArgument):
-        await ctx.send('I could not find that member...')
+# Import
+from flask import Flask, render_template,request, redirect
 
 
 
+app = Flask(__name__)
+
+# Página de contenidos en ejecución
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-        
-token = ''
-bot.run(token)
+# Habilidades dinámicas
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+
+    return render_template('index.html', 
+    button_python=button_python,
+    button_discord=button_discord,
+    button_html=button_html,
+    button_db=button_db
+    )   
+
+
+@app.route('/submit', methods=['POST'])
+def formulario():
+    email = request.form.get('email')
+    text = request.form.get('text')
+
+    with open ('data.txt','a') as file:
+        file.write(f'Email: {email}\n')
+        file.write(f'Text: {text}\n\n')
+
+    return render_template('index.html',
+    text=text,
+    email=email 
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
